cea055ca228b10556cddf7b40698f01fa3e353aa
Sibel Cidem <sibsel_cidem@hotmail.com>
2020-07-29 05:58:16 -0400

    fixed the bugs at service worker and added link to html; working

--- public/indexedDB.js
+++ public/indexedDB.js
@@ -13,6 +13,43 @@ const request = window.indexedDB.open("budget", 1);
           checkDatabase();      
         }
     };
+    request.onerror = function(event) {
+        console.log("Error! " + event.target.errorCode);
+      };
+
+    function saveRecord(record) {
+        const transaction = db.transaction(["pending"], "readwrite");
+
+        const store = transaction.objectStore("pending");
+        store.add(record);
+    }
+
+    function checkDatabase() {
+        const transaction = db.transaction(["pending"], "readwrite");
+        const store = transaction.objectStore("pending");
+        const getAll = store.getAll();
+
+        getAll.onsuccess = function () {
+            if (getAll.result.length > 0) {
+                fetch("/api/transaction/bulk", {
+                    method: "POST",
+                    body: JSON.stringify(getAll.result),
+                    headers: {
+                        Accept: "application/json, text/plain, */*",
+                        "Content-Type": "application/json"
+                    }
+                })
+                .then(response => response.json())
+                .then(() => {
+                    const transaction = db.transaction(["pending"], "readwrite");
+                    const store = transaction.objectStore("pending");
+                    store.clear();
+                });
+                
+            }
+        }
+    }
+
 
     
     window.addEventListener("online", checkDatabase);
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,46 +1,51 @@
 const FILES_TO_CACHE = [
     "/",
+    "/indexedDB.js",
     "/index.js",
     "styles.css",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
   ];
   
-  const PRECACHE = "precache-v1";
-  const RUNTIME = "runtime";
+  const PRECACHE = "static-files-v1";
+  const DATA_CACHE_NAME = "data-cache-v2";
   
   self.addEventListener("install", event => {
     event.waitUntil(
       caches.open(PRECACHE)
         .then(cache => cache.addAll(FILES_TO_CACHE))
-        .then(self.skipWaiting())
-    );
-  });
-  
-  self.addEventListener("activate", event => {
-    const currentCaches = [PRECACHE, RUNTIME];
-    event.waitUntil(
-      caches.keys().then(cacheNames => {
-        return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
-      }).then(cachesToDelete => {
-        return Promise.all(cachesToDelete.map(cacheToDelete => {
-          return caches.delete(cacheToDelete);
-        }));
-      }).then(() => self.clients.claim())
     );
   });
   
   self.addEventListener("fetch", event => {
+      if (event.request.url.includes("/api/")) {
+          console.log("called   " + event.request.url)
+          event.respondWith(
+              caches.open(DATA_CACHE_NAME).then(cache => {
+                  return fetch(event.request)
+                  .then(response => {
+                      if (response.status === 200) {
+                          cache.put(event.request.url, response.clone());
+
+                      }
+                      return response;
+                  })
+                  .catch(err => {
+                    return cache.match(event.request);
+                  });
+              }).catch(err => console.log(err))
+          );
+          return;
+      }
       event.respondWith(
           fetch(event.request).catch(function () {
-              return caches.match(event.request).then(function (cachedResponse) {
-                  if (cachedResponse) {
-                      return cachedResponse;
+              return caches.match(event.request).then(function (response) {
+                  if (response) {
+                      return response;
                   } else if (event.request.headers.get("accept").includes("text/html")) {
-                  return caches.match("/index.html");
+                      return caches.match("/")
                   }
-              })
+              });
           })
-      )
-   
-  });
-
- 
+      );
+});
