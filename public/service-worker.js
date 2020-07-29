fd45dd807dfb4a82cf8e0b382fcc12784c78e6a7
Sibel Cidem <sibsel_cidem@hotmail.com>
2020-07-29 04:42:42 -0400

    prepared service-worker.js

---
+++ public/service-worker.js
@@ -0,0 +1,44 @@
+const FILES_TO_CACHE = [
+    "/",
+    "/index.js",
+    "styles.css",
+  ];
+  
+  const PRECACHE = "precache-v1";
+  const RUNTIME = "runtime";
+  
+  self.addEventListener("install", event => {
+    event.waitUntil(
+      caches.open(PRECACHE)
+        .then(cache => cache.addAll(FILES_TO_CACHE))
+        .then(self.skipWaiting())
+    );
+  });
+  
+  self.addEventListener("activate", event => {
+    const currentCaches = [PRECACHE, RUNTIME];
+    event.waitUntil(
+      caches.keys().then(cacheNames => {
+        return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+      }).then(cachesToDelete => {
+        return Promise.all(cachesToDelete.map(cacheToDelete => {
+          return caches.delete(cacheToDelete);
+        }));
+      }).then(() => self.clients.claim())
+    );
+  });
+  
+  self.addEventListener("fetch", event => {
+      event.respondWith(
+          fetch(event.request).catch(function () {
+              return caches.match(event.request).then(function (cachedResponse) {
+                  if (cachedResponse) {
+                      return cachedResponse;
+                  } else if (event.request.headers.get("accept").includes("text/html")) {
+                  return caches.match("/index.html");
+                  }
+              })
+          })
+      )
+   
+  });
