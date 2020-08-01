6b33c7801ac2ae2956cc54616ade033ac5898378
Sibel Cidem <sibsel_cidem@hotmail.com>
2020-08-01 11:32:11 -0400

    heroku

--- public/indexDb.js
+++ public/indexDb.js
@@ -1,24 +1,14 @@
-const indexedDB =
-    window.indexedDB ||
-    window.mozIndexedDB ||
-    window.webkitIndexedDB ||
-    window.msIndexedDB ||
-    window.shimIndexedDB;
-
 let db;
-// create a new db request for a "budget" database.
-const request = indexedDB.open("budget", 1);
+
+const request = window.indexedDB.open("budget", 1);
 
 request.onupgradeneeded = function (event) {
-    // create object store called "pending" and set autoIncrement to true
     const db = event.target.result;
     db.createObjectStore("pending", { autoIncrement: true });
 };
 
 request.onsuccess = function (event) {
     db = event.target.result;
-
-    // check if app is online before reading from db
     if (navigator.onLine) {
         checkDatabase();
     }
@@ -29,22 +19,16 @@ request.onerror = function (event) {
 };
 
 function saveRecord(record) {
-    // create a transaction on the pending db with readwrite access
     const transaction = db.transaction(["pending"], "readwrite");
 
-    // access your pending object store
     const store = transaction.objectStore("pending");
 
-    // add record to your store with add method.
     store.add(record);
 }
 
 function checkDatabase() {
-    // open a transaction on your pending db
     const transaction = db.transaction(["pending"], "readwrite");
-    // access your pending object store
     const store = transaction.objectStore("pending");
-    // get all records from store and set to a variable
     const getAll = store.getAll();
 
     getAll.onsuccess = function () {
@@ -59,81 +43,13 @@ function checkDatabase() {
             })
                 .then(response => response.json())
                 .then(() => {
-                    // if successful, open a transaction on your pending db
                     const transaction = db.transaction(["pending"], "readwrite");
 
-                    // access your pending object store
                     const store = transaction.objectStore("pending");
-
-                    // clear all items in your store
                     store.clear();
                 });
         }
     };
 }
 
-// listen for app coming back online
 window.addEventListener("online", checkDatabase);
-// let db;
-// const request = window.indexedDB.open("budget", 1);
-// request.onupgradeneeded = event => {
-//     const db = event.target.result;
-//     db.createObjectStore("pending", { autoIncrement: true });
-// };
-
-
-// request.onsuccess = event => {
-//     db = event.target.result;
-
-//     if (navigator.onLine) {
-//         checkDatabase();
-//     }
-// };
-// request.onerror = event => {
-//     console.log("Error! " + event.target.errorCode);
-// };
-
-// saveRecord = record => {
-//     const transaction = db.transaction(["pending"], "readwrite");
-
-//     const store = transaction.objectStore("pending");
-//     store.add(record);
-// }
-
-// checkDatabase = () => {
-//     const transaction = db.transaction(["pending"], "readwrite");
-//     const store = transaction.objectStore("pending");
-//     const getAll = store.getAll();
-
-//     getAll.onsuccess = () => {
-//         if (getAll.result.length > 0) {
-//             fetch("/api/transaction/bulk", {
-//                 method: "POST",
-//                 body: JSON.stringify(getAll.result),
-//                 headers: {
-//                     Accept: "application/json, text/plain, */*",
-//                     "Content-Type": "application/json"
-//                 }
-//             })
-//                 .then(response => response.json())
-//                 .then(() => {
-//                     const transaction = db.transaction(["pending"], "readwrite");
-//                     const store = transaction.objectStore("pending");
-//                     store.clear();
-//                 });
-
-//         }
-//     }
-// }
-
-
-
-// window.addEventListener("online", checkDatabase);
-
-
-
-
-
-
-
-
